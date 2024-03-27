00faff096e8c61ee6056f82c204658a825bc13e6
hamaguchi <93605959+LeandroHamaguchi@users.noreply.github.com>
2024-03-27 19:02:39 -0400

    Created function structure on analyser.py

---
+++ analyser.py
@@ -0,0 +1,27 @@
+"""
+File that runs the main program
+"""
+
+
+def get_user_data(input: str) -> None:
+    """
+    Gets user's input (i.e. Franchise name).
+    """
+
+
+def calculate_score(data: dict) -> float:
+    """
+    Calculates and returns score for each Franchise based on intangible and tangible data.
+    """
+
+
+def visualize_map():
+    """
+    Creates a map visualization of the region where the Franchises and the Landmarks are located.
+    """
+
+
+def visualize_square_graph():
+    """
+    Creates a square graph visualization of the intangible Franchise data.
+    """
