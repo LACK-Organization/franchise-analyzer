10a9979f0ece60cc0e400834f9db5bb5dae056ce
AryanNair-24 <aryan.nair@mail.utoronto.ca>
2024-03-27 19:55:46 -0400

    added descriptions and preconditions for some of the functions

--- analyser.py
+++ analyser.py
@@ -5,19 +5,37 @@ File that runs the main program
 
 def get_user_data(input: str) -> None:
     """
-    Gets user's input (i.e. Franchise name).
+    Gets user's input (i.e. Franchise location).
+
+    This function will take the user's input, which will consist of the locations of the franchises they wish to analyze
+    and compare. The function will find the franchises from the dtataset using these locations.
+
+    Preconditions:
+        - input != ''
+        - all(i.isalnum() or i == '-' for i in input)
+        - input is a valid franchise location
     """
 
 
-def calculate_score(data: dict) -> float:
+def calculate_score(location1_data: dict, location2_data: dict) -> tuple[float]:
     """
     Calculates and returns score for each Franchise based on intangible and tangible data.
+
+    Tangible data includes physical/locational factors such as proximity to public transit, landmarks nearby, ...
+    TODO (To be filled)
+
+    Intangible data includes factors like customer reviews, customer service, infrastructure, number of
+    daily customers, ... TODO (To be filled)
+
+    Preconditions:
+        TODO: Fill this in
     """
 
 
 def visualize_map():
     """
-    Creates a map visualization of the region where the Franchises and the Landmarks are located.
+    Creates a map visualization of the region we're considering, i.e., where the Franchises, Transit points and the
+    Landmarks are located.
     """
 
 
