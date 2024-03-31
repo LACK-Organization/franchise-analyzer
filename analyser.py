efbfb233cabb0810d3f3a99470feaf48d3e64973
codymavok <85233381+codymavok@users.noreply.github.com>
2024-03-31 19:35:54 -0400

    Function added to get the vertex data

--- analyser.py
+++ analyser.py
@@ -1,7 +1,53 @@
+import csv
 """
 File that runs the main program
 TODO: Finish file dosctring
 """
+
+def data_collector(datafile: str, name: str, type: str) -> dict:
+    """Return the data associtated with the vertex"""
+    with open(datafile, 'r') as file1:
+        reader = csv.reader(file1)
+        data_mapping = {}
+        for row in reader:
+            if str(row[2]) == name and row[0] == type:
+                if str(row[0]) == 'MCD':
+                    data_mapping['Type'] = str(row[0])
+                    data_mapping['Cluster'] = int(row[1])
+                    data_mapping['Name'] = str(row[2])
+                    data_mapping['Vehicular Traffic'] = int(row[3])
+                    data_mapping['Pedestrian Traffic'] = int(row[4])
+                    data_mapping['Bike Traffic'] = int(row[5])
+                    data_mapping['Reviews'] = int(row[6])
+                    data_mapping['Crime Rate'] = int(row[7])
+                    data_mapping['Operating Hours'] = int(row[8])
+                    data_mapping['Drive Through'] = int(row[9])
+                    data_mapping['Wifi'] = int(row[10])
+                elif str(row[0]) == 'OtherRestaurant':
+                    data_mapping['Type'] = str(row[0])
+                    data_mapping['Cluster'] = int(row[1])
+                    data_mapping['Name'] = str(row[2])
+                    data_mapping['Reviews'] = int(row[3])
+                    data_mapping['Client Similarity'] = int(row[4])
+                elif str(row[0]) == 'Landmark':
+                    data_mapping['Type'] = str(row[0])
+                    data_mapping['Cluster'] = int(row[1])
+                    data_mapping['Name'] = str(row[2])
+                    data_mapping['Significance'] = int(row[3])
+                elif str(row[0]) == 'Intersection':
+                    data_mapping['Type'] = str(row[0])
+                    data_mapping['Cluster'] = int(row[1])
+                    data_mapping['Name'] = str(row[2])
+                    data_mapping['Bike Per Car Ratio'] = str(row[3])
+                    data_mapping['Vehicular Traffic'] = str(row[4])
+                    data_mapping['Pedestrian Traffic Traffic'] = str(row[5])
+                else:
+                    data_mapping['Type'] = str(row[0])
+                    data_mapping['Cluster'] = int(row[1])
+                    data_mapping['Name'] = str(row[2])
+                    data_mapping['GoogleReviews(REVIEWxREVNUM)'] = int(row[3])
+    return data_mapping
+
 
 
 def get_franchise_locations(location1: str, location2: str) -> None:
@@ -54,4 +100,4 @@ location1 = input("Write the name of the first location: ").strip()
 location2 = input("\nWrite the name of the second location: ").strip()
 get_franchise_locations(location1, location2)
 
-while not state:
+# while not state:
