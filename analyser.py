5f4d9bfd6e9c798c0ebc221db74c8ee36c6bbed3
codymavok <85233381+codymavok@users.noreply.github.com>
2024-03-31 20:43:42 -0400

    Moved data collector to main.py

--- analyser.py
+++ analyser.py
@@ -4,49 +4,7 @@ File that runs the main program
 TODO: Finish file dosctring
 """
 
-def data_collector(datafile: str, name: str, type: str) -> dict:
-    """Return the data associtated with the vertex"""
-    with open(datafile, 'r') as file1:
-        reader = csv.reader(file1)
-        data_mapping = {}
-        for row in reader:
-            if str(row[2]) == name and row[0] == type:
-                if str(row[0]) == 'MCD':
-                    data_mapping['Type'] = str(row[0])
-                    data_mapping['Cluster'] = int(row[1])
-                    data_mapping['Name'] = str(row[2])
-                    data_mapping['Vehicular Traffic'] = int(row[3])
-                    data_mapping['Pedestrian Traffic'] = int(row[4])
-                    data_mapping['Bike Traffic'] = int(row[5])
-                    data_mapping['Reviews'] = int(row[6])
-                    data_mapping['Crime Rate'] = int(row[7])
-                    data_mapping['Operating Hours'] = int(row[8])
-                    data_mapping['Drive Through'] = int(row[9])
-                    data_mapping['Wifi'] = int(row[10])
-                elif str(row[0]) == 'OtherRestaurant':
-                    data_mapping['Type'] = str(row[0])
-                    data_mapping['Cluster'] = int(row[1])
-                    data_mapping['Name'] = str(row[2])
-                    data_mapping['Reviews'] = int(row[3])
-                    data_mapping['Client Similarity'] = int(row[4])
-                elif str(row[0]) == 'Landmark':
-                    data_mapping['Type'] = str(row[0])
-                    data_mapping['Cluster'] = int(row[1])
-                    data_mapping['Name'] = str(row[2])
-                    data_mapping['Significance'] = int(row[3])
-                elif str(row[0]) == 'Intersection':
-                    data_mapping['Type'] = str(row[0])
-                    data_mapping['Cluster'] = int(row[1])
-                    data_mapping['Name'] = str(row[2])
-                    data_mapping['Bike Per Car Ratio'] = str(row[3])
-                    data_mapping['Vehicular Traffic'] = str(row[4])
-                    data_mapping['Pedestrian Traffic Traffic'] = str(row[5])
-                else:
-                    data_mapping['Type'] = str(row[0])
-                    data_mapping['Cluster'] = int(row[1])
-                    data_mapping['Name'] = str(row[2])
-                    data_mapping['GoogleReviews(REVIEWxREVNUM)'] = int(row[3])
-    return data_mapping
+
 
 
 
